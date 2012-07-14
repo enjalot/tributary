c0245d16817f5bfe70ff0678d347317352094ae4
Ian Johnson <enjalot@gmail.com>
2012-07-14 16:54:49 -0700

    fix cypress screen size bug

--- static/views/cypress.js
+++ static/views/cypress.js
@@ -1,11 +1,11 @@
 (function() {
 
     tributary.clear = function() {
-        var sw = parseInt(d3.select("#display").style("width"));
-        var sh = parseInt(d3.select("#display").style("height"));
-        tributary.canvas.width = sw;
-        tributary.canvas.height = sh;
-        tributary.ctx.clearRect(0, 0, sw, sh);
+        //var sw = parseInt(d3.select("#display").style("width"));
+        //var sh = parseInt(d3.select("#display").style("height"));
+        tributary.canvas.width = tributary.sw;
+        tributary.canvas.height = tributary.sh;
+        tributary.ctx.clearRect(0, 0, tributary.sw, tributary.sh);
 
     }
     tributary.Cypress = tributary.Tributary.extend({
