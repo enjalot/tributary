93e84e4e3a12ad7c429d5736684dc467616a09c1
Evan Bollig <bollig@gmail.com>
2012-09-06 04:29:49 -0400

    Got the forking on track

--- app.py
+++ app.py
@@ -218,13 +218,93 @@ def github_authenticated():
 # make js send gist id for fork or save unless its a fresh gist
 # if fresh gist disable fork button
 
+# Request a brand new Gist and return the Gist ID.
+def fork__req_new(old_gist_id, data, token=None):
+    url = 'https://api.github.com/gists'
+    data = data.encode('utf-8')
 
-def save(id, data, token=None):
+    old_data = json.loads(data)
+    if old_gist_id is None:
+        markdown = "No Previous Inlet"
+    else:
+        markdown = "<br/><a href=\"https://gist.github.com/" + old_gist_id + "\">Previous Gist #" + old_gist_id  + "</a>"
+    old_data["files"]["_.md"] = {"content": markdown}
+    data = json.dumps(old_data)
+    data.encode('utf-8')
+
+    headers = {'content-type': 'application/json; charset=utf-8', 'accept': 'application/json', 'encoding':'UTF-8'}
+    if token is not None:
+        headers['Authorization'] = 'token ' + token
+        url = url.encode('utf-8')
+        req = urllib2.Request(url, data, headers=headers)
+    else:
+        req = urllib2.Request(url, data, headers=headers)
+
+    response = urllib2.urlopen(req)
+    ret = response.read()
+
+    gist = json.loads(ret)
+
+    return gist
+
+# Update an existing Gist
+def save__req_update(old_gist_id, new_gist_id, data, token=None):
     #print "ID", id
+#    print "old_gist_id: ", old_gist_id
+#    print "new_gist_id: ", new_gist_id
+
+    url = 'https://api.github.com/gists/' + new_gist_id
+
+    data = data.encode('utf-8')
+#    print "DATA:",data
+#    print "TOKEN: ",token
+
+    old_data = json.loads(data)
+    markdown = "<a href=\"https://gist.github.com/" + new_gist_id + "\">Current Gist #" + new_gist_id + "</a>"
+    if old_gist_id is not None:
+        if old_data.get("_.md",{}):
+            markdown = markdown + "<br/>" + old_data["files"]["_.md"]["content"]
+        else:
+            markdown = markdown + "<br/> <a href=\"" + old_gist_id + "\">Previous Gist #" + old_gist_id + "</a>"
+    else:
+        markdown = markdown + "<br/>----"
+
+#    print "Updated MARKDOWN=",markdown
+
+    old_data["files"]["_.md"] = {"content": markdown}
+    data = json.dumps(old_data)
+    data.encode('utf-8')
+
+    headers = {'content-type': 'application/json; charset=utf-8', 'accept': 'application/json', 'encoding':'UTF-8'}
+    url = url.encode('utf-8')
+    print "URL= ", url
+    if token is not None:
+        headers['Authorization'] = 'token ' + token
+        req = urllib2.Request(url, data, headers=headers)
+    else:
+        req = urllib2.Request(url, data, headers=headers)
+
+    # To to updating Gist (this applies a patch rather than creating a new Gist)
+    req.get_method = lambda: 'PATCH'
+
+    response = urllib2.urlopen(req)
+    ret = response.read()
+
+    gist = json.loads(ret)
+
+    return gist
+
+
+
+def save(old_gist_id, new_gist_id, data, token=None):
+    #print "ID", id
+    print "old_gist_id: ", old_gist_id
+    print "new_gist_id: ", new_gist_id
+
     #if id, send a patch
-    if(id is not None):
+    if(new_gist_id is not None):
         #TODO: check id is a valid id?
-        url = 'https://api.github.com/gists/' + id
+        url = 'https://api.github.com/gists/' + new_gist_id
     else:
         #if not id create a new gist
         url = 'https://api.github.com/gists'
@@ -232,17 +312,25 @@ def save(id, data, token=None):
     #code = json.loads(request.values.get("gist"))
     #print code
     data = data.encode('utf-8')
-    print data
+    print "DATA:",data
+    print "Old ID:",old_gist_id
+    print "New ID: ",new_gist_id
+    print "TOKEN: ",token
 
     # We want to modify the gist by adding a custom markdown file
     gist_data = json.loads(data)
-    markdown = "<a href=\"/" + id + "/" + token + "\"> Load /" + id + "/" + token + "</a>"
+    if token is None and new_gist_id is None:
+        markdown = "No Previous Gist"
+    else:
+        markdown = "<a href=\"/" + old_gist_id + "\"> Load /" + old_gist_id + "/" + "</a>"
+    print markdown
+
     if gist_data.get("_.md",{}):
         print "Updating"
-        gist_data["files"]["_.md"]["content"] = "NEW" + markdown
-    else:
-        print "Creating"
-        gist_data["files"]["_.md"] = {"content": markdown}
+    #    gist_data["files"]["_.md"]["content"] = markdown
+    #else:
+    #    print "Creating"
+    #    gist_data["files"]["_.md"] = {"content": markdown}
 
     # Once we've updated the json object we need it serialized to send
     data = json.dumps(gist_data)
@@ -264,7 +352,7 @@ def save(id, data, token=None):
         req = urllib2.Request(url, data, headers=headers)
 
     #to save over a gist
-    if(id is not None):
+    if(new_gist_id is not None):
         #print "PATCH", url
         req.get_method = lambda: 'PATCH'
 
@@ -284,48 +372,94 @@ def save_endpoint(id=None):
     #from urlparse import urlparse
     #parsed = urlparse('http://example.com')
     #print parsed.hostname
-    ufile = urllib2.urlopen(request.url)
-    baseurl = ufile.geturl()
-    print baseurl
+    #ufile = urllib2.urlopen(request.url)
+    #baseurl = ufile.geturl()
+    #print baseurl
     data = request.values.get("gist")
     token = session.get("access_token", None)
-    return save(id, data, token)
+    return save(id, id, data, token)
 
 
 #Save a tributary to a gist
 @app.route("/tributary/fork", methods=["POST"])
-@app.route("/tributary/fork/<id>", methods=["POST"])
-def fork_endpoint(id=None):
+@app.route("/tributary/fork/<prev_gist_id>", methods=["POST"])
+def fork_endpoint(prev_gist_id=None):
     #TODO: check id is valid
+#    print "ENTER ENDPOINT"
+#    print "prev_gist_id= ",prev_gist_id
 
+    # Data is the content to patch the gist with
     data = request.values.get("gist")
     data = data.encode('utf-8')
 
     token = session.get("access_token", None)
     userid = session.get("userid", None)
+
+#    print 'userid=' , userid
+#    print 'token=' , token
+    #if (token is not None) or (prev_gist_id is not None):
+    old_data = fetch_gist_content(prev_gist_id, token)
+    print "Old data",old_data
+
+#    print json.loads(data).get("user", {})
     gist_userid = json.loads(data).get("user", {}).get("id", None)
 
-    #print 'gist_userid=' , gist_userid
-    #print 'userid=' , userid
-    #print 'token=' , token
+#    print 'gist_userid=' , gist_userid
 
-    if(id is None or token is None):
-        return save(None, data, token)
-    #if user doesn't own this gist, just fork it
-    elif(userid == gist_userid):
-        newgist = fork(id, token)
-        resp = make_response(json.dumps(newgist), 200)
-        resp.headers['Content-Type'] = 'application/json'
-        return resp
+    newgist = None
+
+    if(prev_gist_id is None):
+        # Starting from zero history
+        print "NO GIST"
+        # Fork with None for history
+        newgist = fork__req_new(None, data, token)
+    elif(userid is None):
+        # if user doesn't own this gist, just fork it
+        print "ANON forks SELF/USER"
+        newgist = fork__req_new(prev_gist_id, data, token)
     else:
-        #hacky shit. github won't let me fork a gist a user already owns
-        #first we make anon fork
-        anonid = fork(id)["id"]
-        #then we fork that with our account
-        newid = fork(anonid, token)["id"]
-        #then we save over with the original data
-        return save(newid, data, token)
+        # User owns gist, create a new fork
+        print "USER forks SELF/ANON"
+        # Fork the Gist (with our Username to get a new ID) and update it with the history of
+        # forks
+        anon_gist_id = fork__req_new(prev_gist_id, data, token)["id"]
+        newgist = save__req_update(prev_gist_id, anon_gist_id, data, token)
+#    print "NEW GIST: ", newgist
+    resp = make_response(json.dumps(newgist), 200)
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
 
+def fetch_gist_content(id, token=None):
+    if id is None:
+        return None
+    url = 'https://api.github.com/gists/' + id
+    #need data to make this a post request
+    #data = None
+    data = "{}"
+    headers = {
+            'content-type': 'application/json; charset=utf-8',
+            'accept': 'application/json',
+            'encoding':'UTF-8'
+    }
+    if token is not None:
+        #authenticate the request
+        headers['Authorization'] = 'token ' + token
+        #data = "{authenticity_token:" + token + "}"
+        #data = "{access_token:" + token + "}"
+        #print "LOGGED IN, using TOKEN", token
+        #url += "?access_token="+token
+        #url = url.encode('utf-8')
+        req = urllib2.Request(url, headers=headers)
+        #req = urllib2.Request(url, data)
+    else:
+        print "NOT LOGGED IN"
+        req = urllib2.Request(url, headers=headers)
+
+    response = urllib2.urlopen(req)
+
+    ret = response.read()
+    gist = json.loads(ret)
+    return gist
 
 def fork(id, token=None):
     #print "FORKING", id
