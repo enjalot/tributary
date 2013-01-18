ba4af741f5ff88f3338fb209ca84598e4cf3a317
Ian Johnson <enjalot@gmail.com>
2013-01-17 17:56:07 -0800

    log the bash scripts

--- reports/backup.sh
+++ reports/backup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+(
 NOW=$(date +"%Y-%m-%d-%H:%M")
-mongodump --db tributary --out $NOW
+mongodump --db tributary --out $OUT/$NOW
+) 2>&1 | tee -a $OUT/reports.log
--- reports/reports.sh
+++ reports/reports.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #pass in $DIR when you call this so you can be sure its executed from the root trbiutary directory
 #i.e. DIR=~/code/tributary reports.sh
+(
 cd $DIR
 node reports/aggregators/visitsByGist.js
 node reports/aggregators/inletsByUser.js
+) 2>&1 | tee -a $DIR/reports.log
