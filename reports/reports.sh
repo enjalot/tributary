293db7ec32ba49478f755bd6bc25fc11ab06b3e8
Ian Johnson <enjalot@gmail.com>
2013-01-17 17:38:43 -0800

    update path for reports.sh

--- reports/reports.sh
+++ reports/reports.sh
@@ -2,5 +2,5 @@
 #pass in $DIR when you call this so you can be sure its executed from the root trbiutary directory
 #i.e. DIR=~/code/tributary reports.sh
 cd $DIR
-node reports/visitsByGist.js
-node reports/inletsByUser.js
+node reports/aggregators/visitsByGist.js
+node reports/aggregators/inletsByUser.js
