8514aaadce50ee563a58ad900500ec334762497f
Ian Johnson <enjalot@gmail.com>
2013-01-17 18:00:19 -0800

    hardcode some paths for bash scripts

--- reports/backup.sh
+++ reports/backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 (
 NOW=$(date +"%Y-%m-%d-%H:%M")
+OUT=/home/ubuntu/backup
 mongodump --db tributary --out $OUT/$NOW
-) 2>&1 | tee -a $OUT/reports.log
+) 2>&1 | tee -a $OUT/backup.log
--- reports/reports.sh
+++ reports/reports.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#pass in $DIR when you call this so you can be sure its executed from the root trbiutary directory
+#TODO: make it so you can pass in $DIR when you call this so you can be sure its executed from the root trbiutary directory
 #i.e. DIR=~/code/tributary reports.sh
 (
+DIR=/home/ubuntu/tributary
 cd $DIR
 node reports/aggregators/visitsByGist.js
 node reports/aggregators/inletsByUser.js
