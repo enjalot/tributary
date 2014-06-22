b6a56fd740e14e452518af1250f166644d948fb2
Ian Johnson <enjalot@gmail.com>
2014-06-22 13:41:44 -0700

    update backup process

--- reports/backup.sh
+++ reports/backup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-OUT=/home/ubuntu/backup
+BACKUP=/home/ubuntu/backup
+OUT=/tmp
+NOW=$(date +"%Y-%m-%d-%H_%M")
 (
-NOW=$(date +"%Y-%m-%d-%H:%M")
 mongodump --db tributary --out "$OUT/$NOW"
 ) 2>&1 | tee -a "$OUT/backup.log"
+
+TAR="$OUT/$NOW.tar.gz"
+tar czvf "$TAR" "$OUT/$NOW"
+
+coffee $BACKUP/s3.coffee --method upload -f $TAR -p "$NOW.tar.gz"
+
+rm -rf "$OUT/$NOW"
+rm -rf "$TAR"
